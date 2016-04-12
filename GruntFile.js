91e1710cb7ec08dc0e37711813c94b61a5141764
Chima <xhimat123@gmail.com>
2016-04-12 17:52:31 +0100

    refactored code into standard grunt way

---
+++ GruntFile.js
@@ -0,0 +1,20 @@
+module.exports = function(grunt) {
+  grunt.initConfig(
+    {
+     couchdb:
+     {
+     url :'http://127.0.0.1:5984',
+     user : 'super',
+     password : 'super',
+     bootstrap: 
+     {
+     dir : 'couchdb/bootstrap'
+     },
+     fixtures : {
+     
+     }
+     }
+    });
+  grunt.loadTasks('tasks')
+  
+};
